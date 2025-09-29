498954fc9ddac7cf7ce408607986a83eee294883
kotlovyim <kotlovyim@gmail.com>
2025-09-29 20:12:46 +0300

    feat: implement authentication views and layout with sign-in and sign-up forms

---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,174 @@
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { OctagonAlertIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
+const formSchema = z.object({
+    email: z.email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export const SignInView = () => {
+    const router = useRouter();
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        setError(null);
+        setLoading(true);
+        const { error } = await authClient.signIn.email(
+            { email: data.email, password: data.password },
+            {
+                onSuccess: () => router.push("/dashboard"),
+                onError: ({ error }) => setError(error.message),
+                onSettled: () => setLoading(false),
+            }
+        );
+    };
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: { email: "", password: "" },
+    });
+    return (
+        <div className="flex flex-col gap-6">
+            <Card className="overflow-hidden p-0">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <Form {...form}>
+                        <form
+                            className="p-6 md:p-8"
+                            onSubmit={form.handleSubmit(onSubmit)}
+                        >
+                            <div className="flex flex-col gap-6">
+                                <div className="flex flex-col items-center text-center">
+                                    <h1 className="text-2xl font-bold">
+                                        Welcome Back!
+                                    </h1>
+                                    <p className="text-muted-foreground text-balance">
+                                        Login to your account
+                                    </p>
+                                </div>
+                                <div className="grid gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        {...field}
+                                                        type="email"
+                                                        placeholder="me@example.com"
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Password</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        {...field}
+                                                        type="password"
+                                                        placeholder="••••••••"
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                {!!error && (
+                                    <Alert className="bg-destructive/10 border-none">
+                                        <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                                        <AlertTitle>{error}</AlertTitle>
+                                    </Alert>
+                                )}
+                                <Button
+                                    type="submit"
+                                    className="w-full"
+                                    disabled={loading}
+                                >
+                                    {loading ? "Loading..." : "Sign In"}
+                                </Button>
+                                <div
+                                    className="after:border-border relative text-center text-sm after:absolute
+                                after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
+                                >
+                                    <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                        Or continue with
+                                    </span>
+                                </div>
+                                <div className="grid grid-cols-2 gap-4">
+                                    <Button
+                                        variant="outline"
+                                        className="w-full"
+                                        type="button"
+                                    >
+                                        Google
+                                    </Button>
+                                    <Button
+                                        variant="outline"
+                                        className="w-full"
+                                        type="button"
+                                    >
+                                        GitHub
+                                    </Button>
+                                </div>
+                                <div className="text-center text-sm text-muted-foreground">
+                                    Don&apos;t have an account?{" "}
+                                    <Link
+                                        href="/sign-up"
+                                        className="text-primary hover:underline underline-offset-4"
+                                    >
+                                        Sign up
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </Form>
+                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                        <Image
+                            src="/logo.svg"
+                            alt="MeetAI"
+                            width={92}
+                            height={92}
+                        />
+                        <p className="text-2xl font-semibold text-white">
+                            MeetAI
+                        </p>
+                    </div>
+                </CardContent>
+            </Card>
+            <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+                By signing in, you agree to our{" "}
+                <Link href="#">Terms of Service</Link> and{" "}
+                <Link href="#">Privacy Policy</Link>.
+            </div>
+        </div>
+    );
+};
