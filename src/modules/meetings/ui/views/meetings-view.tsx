c9d43aac38593dce04f452a3280cfcdacdbfe82b
kotlovyim2026 <kotlovyim@ukr.net>
2026-01-18 14:32:55 +0200

    feat: add data table for meetings and agents, enhance type definitions, and integrate humanize-duration for duration formatting

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,14 +1,26 @@
 "use client";
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
-
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 export const MeetingsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    return (
+        <div className="flex-1 pb-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data={data.items} columns={columns}></DataTable>
+            {data.total === 0 && (
+                <EmptyState
+                    title="Create Your First Meeting"
+                    description="Shedule meetings and invite your AI agents to assist you during calls. Each meeting lets you collaborate, share ideas, and interact with participants in real-time."
+                />
+            )}
+        </div>
+    );
 };
 
 export const MeetingsViewLoading = () => {
