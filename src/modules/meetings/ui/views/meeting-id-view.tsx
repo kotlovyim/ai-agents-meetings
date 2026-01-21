16505351b207e7491367b0970b138e55db68e41b
kotlovyim2026 <kotlovyim@ukr.net>
2026-01-21 10:41:26 +0200

    feat: implement meeting states with active, cancelled, upcoming, and processing components

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -13,6 +13,10 @@ import { useConfirm } from "@/modules/agents/hooks/use-confirm";
 import { toast } from "sonner";
 import { useState } from "react";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props {
     meetingId: string;
@@ -57,6 +61,12 @@ export const MeetingIdView = ({ meetingId }: Props) => {
         }
     };
 
+    const isActive = data.status == "active";
+    const isUpcoming = data.status == "upcoming";
+    const isCancelled = data.status == "cancelled";
+    const isCompleted = data.status == "completed";
+    const isProcessing = data.status == "processing";
+
     return (
         <>
             <UpdateMeetingDialog
@@ -72,7 +82,17 @@ export const MeetingIdView = ({ meetingId }: Props) => {
                     onEdit={() => setMeetingUpdateDialog(true)}
                     onRemove={handleRemoveMeeting}
                 />
-                Meeting ID View for {meetingId}
+                {isActive && <ActiveState meetingId={meetingId} />}
+                {isUpcoming && (
+                    <UpcomingState
+                        meetingId={meetingId}
+                        onCancelMeeting={handleRemoveMeeting}
+                        isCancelling={false}
+                    />
+                )}
+                {isCancelled && <CancelledState />}
+                {isCompleted && <div>Meeting is Completed</div>}
+                {isProcessing && <ProcessingState />}
             </div>
         </>
     );
